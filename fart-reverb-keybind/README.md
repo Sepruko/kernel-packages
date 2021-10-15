811f467a9bf65de9564edfe00596c059039249e6
Winston Sepruko <notexactlymebutmeh@gmail.com>
2021-10-15 13:27:58 +1100

    rename frb to frk

--- fart-reverb-keybind/README.md
+++ fart-reverb-keybind/README.md
@@ -1,4 +1,4 @@
-# Fart Reverb Button
+# Fart Reverb Keybind
 This package adds a global shortcut (`CTRL/CMD+ALT+F`) to *any* Electron app you choose to use it on, and it will play a fart sound with reverb.
 
 This is a meme package, however can serve the purpose of being an example of how to register global shortcuts for the Electron app and use Kernel's `broadcast` module to send and receive events to Electron's different contexts.
--- fart-reverb-keybind/preload.js
+++ fart-reverb-keybind/preload.js
@@ -2,7 +2,7 @@ const broadcast = require("../../electron/dist/kernel.asar/core/broadcast");
 
 module.exports.default = {
   start() {
-    const fart = new Audio("https://raw.githubusercontent.com/Sepruko/kernel-packages/main/fart-reverb-button/assets/fart-reverb.mp3");
+    const fart = new Audio("https://raw.githubusercontent.com/Sepruko/kernel-packages/main/fart-reverb-keybind/assets/fart-reverb.mp3");
 
     broadcast.default.on("play-fart-sound", () => {
       fart.play();
