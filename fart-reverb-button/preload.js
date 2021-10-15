049a3f195f8a0ce3a2c012c262eb0db96fb576a1
Winston Sepruko <notexactlymebutmeh@gmail.com>
2021-10-15 13:05:27 +1100

    switch to preload from renderer

---
+++ fart-reverb-button/preload.js
@@ -0,0 +1,11 @@
+const broadcast = require("../../electron/dist/kernel.asar/core/broadcast");
+
+module.exports.default = {
+  start() {
+    const fart = new Audio("https://raw.githubusercontent.com/Sepruko/kernel-packages/main/fart-reverb-button/assets/fart-reverb.mp3");
+
+    broadcast.default.on("play-fart-sound", () => {
+      fart.play();
+    });
+  }
+}
